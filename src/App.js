e4e3ad53d77cc33c6534cd3d280ff1f02ab6a070
ZeeAzeez <azeezahaazeez@gmail.com>
2024-03-26 17:52:53 +0100

    top categories section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import TopCategories from './components/TopCategories';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <TopCategories />
     </div>
   );
 }
---
+++ src/components/TopCategories/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles.css";
+
+function TopCategories(){
+    return(
+        <div className='top-categories'>
+
+        </div>
+    )
+}
+
+export default TopCategories;
